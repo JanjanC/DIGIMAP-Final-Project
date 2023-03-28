55fbd4cbe67498c02dc2e2e64494a2fd52c81f59
JanjanC <janjan_chua@dlsu.edu.ph>
2023-03-28 23:54:28 +0800

    Implement run python script

--- index.js
+++ index.js
@@ -7,29 +7,25 @@ const app = express();
 app.use(express.static(path.join(__dirname, "/assets")));
 
 app.get("/", function (req, res) {
-    let dataToSend;
-    // spawn new child process to call the python script
-    const python = spawn("python", [path.join(__dirname, "/script.py")]);
-    // collect data from script
-    python.stdout.on("data", function (data) {
-        console.log("Pipe data from python script ...");
-        dataToSend = data.toString();
-        console.log(dataToSend);
-    });
-
-    // in close event we are sure that stream from child process is closed
-    python.on("close", (code) => {
-        console.log(`child process close all stdio with code ${code}`);
-        // send data to browser
-        // console.log(dataToSend);
-    });
-
-    console.log("hi");
-    res.sendFile(path.join(__dirname, "/index.html"));
+  // let dataToSend;
+  // // spawn new child process to call the python script
+  // const python = spawn("python", [path.join(__dirname, "/script.py")]);
+  // // collect data from script
+  // python.stdout.on("data", function (data) {
+  //     console.log("Pipe data from python script ...");
+  //     dataToSend = data.toString();
+  //     console.log(dataToSend);
+  // });
+  // // in close event we are sure that stream from child process is closed
+  // python.on("close", (code) => {
+  //     console.log(`child process close all stdio with code ${code}`);
+  //     // send data to browser
+  //     // console.log(dataToSend);
+  // });
+  // console.log("hi");
+  res.sendFile(path.join(__dirname, "/index.html"));
 });
 
 let listener = app.listen(process.env.PORT || 3000, function () {
-    console.log(
-        "Server running at http://localhost:" + listener.address().port
-    );
+  console.log("Server running at http://localhost:" + listener.address().port);
 });
