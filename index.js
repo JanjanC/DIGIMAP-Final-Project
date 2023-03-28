8b85e139d304bd5d4cf68425532a3dcbc31680cb
JanjanC <janjan_chua@dlsu.edu.ph>
2023-03-28 16:56:58 +0800

    Changed lib to node_modules

--- index.js
+++ index.js
@@ -4,32 +4,34 @@ const path = require("path");
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, "/assets")));
+app.use(express.static(path.join(__dirname, "assets")));
+app.use("/bootstrap", express.static(path.join(__dirname, "node_modules", "bootstrap")));
+app.use("/@popperjs", express.static(path.join(__dirname, "node_modules", "@popperjs")));
+app.use("/jquery", express.static(path.join(__dirname, "node_modules", "jquery")));
+app.use("/font-awesome", express.static(path.join(__dirname, "node_modules", "font-awesome")));
 
 app.get("/", function (req, res) {
-    let dataToSend;
-    // spawn new child process to call the python script
-    const python = spawn("python", [path.join(__dirname, "/script.py")]);
-    // collect data from script
-    python.stdout.on("data", function (data) {
-        console.log("Pipe data from python script ...");
-        dataToSend = data.toString();
-        console.log(dataToSend);
-    });
+	let dataToSend;
+	// spawn new child process to call the python script
+	const python = spawn("python", [path.join(__dirname, "/script.py")]);
+	// collect data from script
+	python.stdout.on("data", function (data) {
+		console.log("Pipe data from python script ...");
+		dataToSend = data.toString();
+		console.log(dataToSend);
+	});
 
-    // in close event we are sure that stream from child process is closed
-    python.on("close", (code) => {
-        console.log(`child process close all stdio with code ${code}`);
-        // send data to browser
-        // console.log(dataToSend);
-    });
+	// in close event we are sure that stream from child process is closed
+	python.on("close", (code) => {
+		console.log(`child process close all stdio with code ${code}`);
+		// send data to browser
+		// console.log(dataToSend);
+	});
 
-    console.log("hi");
-    res.sendFile(path.join(__dirname, "/index.html"));
+	console.log("hi");
+	res.sendFile(path.join(__dirname, "/index.html"));
 });
 
 let listener = app.listen(process.env.PORT || 3000, function () {
-    console.log(
-        "Server running at http://localhost:" + listener.address().port
-    );
+	console.log("Server running at http://localhost:" + listener.address().port);
 });
