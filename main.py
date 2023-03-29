fddbcc72cc59e2146b92db0e19b9ab24d938c3f4
JanjanC <janjan_chua@dlsu.edu.ph>
2023-03-29 15:30:48 +0800

    Project initialization with bootstrap

---
+++ main.py
@@ -0,0 +1,16 @@
+import os
+from flask import Flask, render_template
+
+app = Flask(__name__, template_folder=os.path.join("assets", "templates"), static_folder="assets")
+
+
+@app.route("/")
+def hello_world():
+    return render_template("index.html")
+
+
+#  pass
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
