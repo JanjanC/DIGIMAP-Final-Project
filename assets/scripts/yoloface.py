3eaf3748d43a95eddd4d33b683a7d1563ada2624
JanjanC <janjan_chua@dlsu.edu.ph>
2023-03-29 22:37:54 +0800

    Removed unnecessary code from yolo_face.py

--- assets/scripts/yoloface.py
+++ assets/scripts/yoloface.py
@@ -24,63 +24,48 @@ class YOLOFace:
         blob = cv2.dnn.blobFromImage(img, 1/255, (416, 416), (0, 0, 0), swapRB=True, crop=False)
 
         self.yolo_net.setInput(blob)
-        output_layers_names = self.yolo_net.getUnconnectedOutLayersNames()
-        layerOutputs = self.yolo_net.forward(output_layers_names)
-
-        classes = ['face', 'back']
+        output_layers = self.yolo_net.getUnconnectedOutLayersNames()
+        output_list = self.yolo_net.forward(output_layers)
 
         boxes = []
         confidences = []
         class_ids = []
 
-        for output in layerOutputs:
+        for output in output_list:
             for detection in output:
-                scores = detection[5:]
-                class_id = np.argmax(scores)
-                confidence = scores[class_id]
+                confidence = detection[5]  # only one class (face)
                 if confidence > 0.50:
-                    center_x = int(detection[0]*width)
-                    center_y = int(detection[1]*height)
-                    w = int(detection[2]*width)
-                    h = int(detection[3]*height)
-                    x = int(center_x - (w/2))
-                    y = int(center_y - (h/2))
+                    center_x = int(detection[0] * width)
+                    center_y = int(detection[1] * height)
+                    w = int(detection[2] * width)
+                    h = int(detection[3] * height)
+                    x = int(center_x - (w / 2))
+                    y = int(center_y - (h / 2))
                     boxes.append([x, y, h, w])
                     confidences.append((float(confidence)))
-                    class_ids.append(class_id)
-        indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
+                    class_ids.append(0)
+
+        indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.5)
         face_box = []
-        conf = []
+        face_confidence = []
 
         if len(indexes) > 0:
             for i in indexes.flatten():
-                label = str(classes[class_ids[i]])
-                if label == classes[0]:
-                    face_box.append(boxes[i])
-                    conf.append(confidences[i])
+                face_box.append(boxes[i])
+                face_confidence.append(confidences[i])
 
-        return img, face_box, conf
+        return img, face_box, face_confidence
 
-    def show(self, img, face_box, prediction_list=None, frame_status=False):
+    def show(self, img, face_box, frame_status=False):
         self.img = img
         self.face_box = face_box
-        self.prediction_list = prediction_list
         self.frame_status = frame_status
-        font = cv2.FONT_HERSHEY_PLAIN
-
-        color = (255, 200, 200)
 
         if len(face_box) > 0:
             for i in range(len(face_box)):
                 box = face_box[i]
                 x, y, w, h = box[0], box[1], box[2], box[3]
-                label = "{}".format('face')
-                cv2.rectangle(self.img, (x, y), (x+h, y+w), (255, 255, 255), 3)
-                (text_width, text_height) = cv2.getTextSize(label, font, fontScale=1, thickness=1)[0]
-                text_offset_x, text_offset_y = x, y - 10
-                box_coords = ((text_offset_x, text_offset_y+10), (text_offset_x + text_width + 10, text_offset_y - text_height-10))
-                cv2.rectangle(self.img, box_coords[0], box_coords[1], (0, 0, 0), cv2.FILLED)
-                cv2.putText(self.img, label, (text_offset_x, text_offset_y), font, fontScale=1, color=color, thickness=1)
+                cv2.rectangle(self.img, (x, y), (x + h, y + w), (255, 255, 255), 1)
 
         if self.frame_status == False:
             img = cv2.cvtColor(self.img, cv2.COLOR_BGR2RGB)  # Converting BGR to RGB
