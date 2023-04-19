b3b0adadd0072fce1620630b7e5d68d6fdd38ff6
JanjanC <janjan_chua@dlsu.edu.ph>
2023-04-20 04:01:16 +0800

    Clean up main.py by removing print statements and adding some comments

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import os
 import cv2
-from flask import Flask, Response, render_template, request, redirect
+from flask import Flask, render_template, request, redirect
 from werkzeug.utils import secure_filename
 from assets.scripts.yoloface import YOLOFace
 
 UPLOAD_FOLDER = os.path.join("assets", "uploads")
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+
 if not os.path.exists(UPLOAD_FOLDER):
     os.mkdir(UPLOAD_FOLDER)
 
@@ -20,42 +21,41 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 def index():
     return render_template("index.html")
 
+
+# a function to check if the file extension is valid
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-
 @app.route('/results', methods=['POST'])
 def upload_file():
-    print(request.files)
     # check if the post request has the file part
     if 'filepond' not in request.files:
         return render_template("index.html", error="Failed to Upload File")
     file = request.files['filepond']
 
     # if the user does not select a file, the browser submits an empty file without a filename.
-    print(type(file.stream))
     if file.filename == '':
-        print('No selected file')
         return render_template("index.html", error="No Valid File Uploaded")
+    
+    # invalid file extension
     if not allowed_file(file.filename):
-        print('Invalid file type')
         return render_template("index.html", error="Invalid File Type")
     
+    # a valid file was uploaded
     if file:
-        print('file', file)
         filename = secure_filename(file.filename)
         img_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
         file.save(img_path)
+        # run face detection
         img, box, confidence = model.detect(img_path)
-        print(img)
-        input_img = model.convert_img(img)
-        output = model.show_box(img, box)
-        output_img = model.convert_img(output)
+        input_img = model.convert_img(img) # convert input image to base64
+        output = model.show_box(img, box) # draw bounding box
+        output_img = model.convert_img(output) # convert output image to base64
         if os.path.isfile(img_path):
             os.remove(img_path)
         return render_template("result.html", input_img=input_img, output_img=output_img)
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
