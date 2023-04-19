a420cd23de7c74d86cf7ffec87c83316f284518c
JanjanC <janjan_chua@dlsu.edu.ph>
2023-04-20 04:16:02 +0800

    Clean up yoloface code

--- assets/scripts/yoloface.py
+++ assets/scripts/yoloface.py
@@ -6,35 +6,35 @@ from base64 import b64encode
 
 
 class YOLOFace:
+    # load weight and cfg for the model
     def __init__(self, weight_path, cfg_path):
         self.yolo_net = cv2.dnn.readNet(weight_path, cfg_path)
 
-    def detect(self, image_path=None, frame_status=False, frame_list=None):
+    # detect the faces in the input image
+    def detect(self, image_path):
         self.image_path = image_path
-        self.frame_status = frame_status
-        self.frame_list = frame_list
-
-        if self.image_path != None and self.frame_status == False and self.frame_list == None:
-            self.image_path = image_path
-            img = cv2.imread(self.image_path)
-        else:
-            img = self.frame_list
-
+        
+        # read image
+        img = cv2.imread(self.image_path)
+        
         height, width, _ = img.shape
+
+        # preprocess image to 416x416
         blob = cv2.dnn.blobFromImage(img, 1/255, (416, 416), (0, 0, 0), swapRB=True, crop=False)
 
+        # predict model output
         self.yolo_net.setInput(blob)
         output_layers = self.yolo_net.getUnconnectedOutLayersNames()
         output_list = self.yolo_net.forward(output_layers)
 
         boxes = []
         confidences = []
-        class_ids = []
 
+        # get the bounding boxes and confidence
         for output in output_list:
             for detection in output:
                 confidence = detection[5]  # only one class (face)
-                if confidence > 0.50:
+                if confidence > 0.50: #score threshold of 0.5
                     center_x = int(detection[0] * width)
                     center_y = int(detection[1] * height)
                     w = int(detection[2] * width)
@@ -43,8 +43,8 @@ class YOLOFace:
                     y = int(center_y - (h / 2))
                     boxes.append([x, y, h, w])
                     confidences.append((float(confidence)))
-                    class_ids.append(0)
 
+        # score threshold of 0.5 and nms threshold of 0.5
         indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
         face_box = []
         face_confidence = []
@@ -56,6 +56,7 @@ class YOLOFace:
 
         return img, face_box, face_confidence
 
+    # draw a bounding box on the image
     def show_box(self, img, face_box):
         self.img = img
         self.face_box = face_box
@@ -68,8 +69,9 @@ class YOLOFace:
 
         return img
     
+    # convert image to base64
     def convert_img(self, img):
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # Converting BGR to RGB
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         img = Image.fromarray(img)
         file_object = io.BytesIO()
         img.save(file_object, 'PNG')
