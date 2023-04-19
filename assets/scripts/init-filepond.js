fd79b1ae4b906640836f72a20c796fb8808e09c2
JanjanC <janjan_chua@dlsu.edu.ph>
2023-04-20 03:43:05 +0800

    Rename filepond to init-filepond

--- assets/scripts/init-filepond.js
+++ assets/scripts/init-filepond.js
@@ -3,6 +3,5 @@ FilePond.registerPlugin(FilePondPluginFileValidateType, FilePondPluginImagePrevi
 const inputElement = document.querySelector('input[type="file"]');
 const pond = FilePond.create(inputElement, {
 	storeAsFile: true,
-	// labelIdle: "<p>Upload Image</p><p>*png, *jpg, or *jpeg</p>",
 	acceptedFileTypes: ["image/jpg", "image/jpeg", "image/png"],
 });
