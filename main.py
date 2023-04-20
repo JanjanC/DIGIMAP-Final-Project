a3ec11882c30bd29680876b2432f8f231563dc8f
Amos-Cacha <amos_cacha@dlsu.edu.ph>
2023-04-20 05:54:43 +0000

    fixed refreshing on result page

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 import cv2
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for
 from werkzeug.utils import secure_filename
 from assets.scripts.yoloface import YOLOFace
 
@@ -27,8 +27,11 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/results', methods=['POST'])
+@app.route('/results', methods=['GET', 'POST'])
 def upload_file():
+    if request.method == 'GET':
+        return redirect(url_for('index'))
+
     # check if the post request has the file part
     if 'filepond' not in request.files:
         return render_template("index.html", error="Failed to Upload File.")
@@ -37,11 +40,11 @@ def upload_file():
     # if the user does not select a file, the browser submits an empty file without a filename.
     if file.filename == '':
         return render_template("index.html", error="No Valid File Uploaded.")
-    
+
     # invalid file extension
     if not allowed_file(file.filename):
         return render_template("index.html", error="Invalid File Type.")
-    
+
     # a valid file was uploaded
     if file:
         filename = secure_filename(file.filename)
@@ -54,7 +57,7 @@ def upload_file():
         output_img = model.convert_img(output) # convert output image to base64
         if os.path.isfile(img_path):
             os.remove(img_path)
-        
+
         if len(box) > 0: #faces detected
             return render_template("result.html", input_img=input_img, output_img=output_img)
         else: # no faces detected
